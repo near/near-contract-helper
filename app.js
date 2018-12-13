46d580a22c54eb894fb63767938c9c18e4a51da5
Illia Polosukhin <ilblackdragon@gmail.com>
2018-12-13 12:18:30 -0800

    Move send transaciton into separate function

--- app.js
+++ app.js
@@ -48,48 +48,50 @@ const hash = async str => {
     return bs58.encode(data);
 };
 
+const submit_transaction_rpc = async (client, method, args) => {
+    const response = await client.request(method, [args])
+    if (response.error) {
+        return response
+    }
+    // TODO(#2): Sign transactions.
+    const res = await client.request('submit_transaction', [{
+        body: response.result.body,
+        sender_sig: [147, 237, 206, 27, 8, 112, 190, 228, 129, 23, 111, 153, 134, 68, 203, 152, 108, 105, 112, 238, 171, 200, 158, 83, 197, 20, 158, 214, 151, 190, 92, 135, 33, 158, 42, 159, 131, 80, 96, 201, 185, 254, 213, 46, 209, 11, 158, 2, 57, 161, 66, 222, 87, 192, 141, 53, 43, 198, 40, 107, 148, 218, 231, 7],
+    }])
+    return res
+}
+
 router.post('/contract', async ctx => {
     const body = ctx.request.body;
     const sender = body.sender || hardcodedSender;
     const nonce = body.nonce || await getNonce(ctx, sender);
     console.log(`Deploing ${body.receiver} contract`);
-    const response = await client.request('deploy_contract', [{
+    const contract_response = await submit_transaction_rpc(client, 'deploy_contract', {
         nonce: nonce,
-        sender_account_id: await hash(sender),
+        sender_account_id: await hash(body.receiver),
         contract_account_id: await hash(body.receiver),
         wasm_byte_array: base64ToIntArray(body.contract),
         public_key: hardcodedKey.public_key
-    }]);
-    checkError(ctx, response);
-    console.log("response", response);
-    // TODO(#2): Sign transactions.
-    const res = await client.request('submit_transaction', [{
-        body: response.result.body,
-        sender_sig: [147, 237, 206, 27, 8, 112, 190, 228, 129, 23, 111, 153, 134, 68, 203, 152, 108, 105, 112, 238, 171, 200, 158, 83, 197, 20, 158, 214, 151, 190, 92, 135, 33, 158, 42, 159, 131, 80, 96, 201, 185, 254, 213, 46, 209, 11, 158, 2, 57, 161, 66, 222, 87, 192, 141, 53, 43, 198, 40, 107, 148, 218, 231, 7],
-    }]);
-    console.log("response", res);
-    ctx.body = res.result;
+    })
+    console.log("response", contract_response);
+    checkError(ctx, contract_response);
+    ctx.body = contract_response.result;
 });
 
 router.post('/contract/:name/:methodName', async ctx => {
     const body = ctx.request.body;
     const sender = body.sender || hardcodedSender;
     const nonce = body.nonce || await getNonce(ctx, sender);
-    const response = await client.request('schedule_function_call', [{
+    const response = await submit_transaction_rpc(client, 'schedule_function_call', {
         nonce: nonce,
         originator_account_id: await hash(sender),
         contract_account_id: await hash(ctx.params.name),
         method_name: ctx.params.methodName,
         args: [body.args]
-    }]);
+    });
     checkError(ctx, response);
-    // TODO(#2): Sign transactions.
-    const res = await client.request('submit_transaction', [{
-        body: response.result.body,
-        sender_sig: [147, 237, 206, 27, 8, 112, 190, 228, 129, 23, 111, 153, 134, 68, 203, 152, 108, 105, 112, 238, 171, 200, 158, 83, 197, 20, 158, 214, 151, 190, 92, 135, 33, 158, 42, 159, 131, 80, 96, 201, 185, 254, 213, 46, 209, 11, 158, 2, 57, 161, 66, 222, 87, 192, 141, 53, 43, 198, 40, 107, 148, 218, 231, 7],
-    }]);
-    console.log("response", res);
-    ctx.body = res.result;
+    console.log("response", response);
+    ctx.body = response.result;
 });
 
 router.post('/contract/view/:name/:methodName', async ctx => {
