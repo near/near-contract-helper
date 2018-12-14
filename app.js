cc7d7fb27d71286d850d8c60b8d97274eefb45fc
Evguenia degtiareva <jane.degtiareva@gmail.com>
2018-12-13 18:04:19 -0800

    [WIP] DO NOT SUBMIT wip to encode args in bson

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+//import { BSON } from 'bsonfy';
 
 const Koa = require('koa');
 const app = new Koa();
 
 const body = require('koa-json-body')
 const cors = require('@koa/cors');
+const BSON = require('bsonfy').BSON;
 
 const hardcodedKey = {
     "public_key":"9AhWenZ3JddamBoyMqnTbp7yVbRuvqAv3zwfrWgfVRJE",
@@ -39,7 +41,7 @@ const base64ToIntArray = base64Str => {
 
 const checkError = (ctx, response) => {
     if (response.error) {
-        ctx.throw(400, `[${response.error.code}] ${response.error.message}: ${response.error.data}`);
+        ctx.throw(400, `[${response.error.code}] ${response.error.message}`);
     }
 };
 
@@ -48,58 +50,72 @@ const hash = async str => {
     return bs58.encode(data);
 };
 
-const submit_transaction_rpc = async (client, method, args) => {
-    const response = await client.request(method, [args])
-    if (response.error) {
-        return response
-    }
-    // TODO(#2): Sign transactions.
-    const res = await client.request('submit_transaction', [{
-        body: response.result.body,
-        sender_sig: [147, 237, 206, 27, 8, 112, 190, 228, 129, 23, 111, 153, 134, 68, 203, 152, 108, 105, 112, 238, 171, 200, 158, 83, 197, 20, 158, 214, 151, 190, 92, 135, 33, 158, 42, 159, 131, 80, 96, 201, 185, 254, 213, 46, 209, 11, 158, 2, 57, 161, 66, 222, 87, 192, 141, 53, 43, 198, 40, 107, 148, 218, 231, 7],
-    }])
-    return res
-}
-
 router.post('/contract', async ctx => {
     const body = ctx.request.body;
     const sender = body.sender || hardcodedSender;
     const nonce = body.nonce || await getNonce(ctx, sender);
-    console.log(`Deploing ${body.receiver} contract`);
-    const contract_response = await submit_transaction_rpc(client, 'deploy_contract', {
+    const response = await client.request('deploy_contract', [{
         nonce: nonce,
         sender_account_id: await hash(sender),
         contract_account_id: await hash(body.receiver),
         wasm_byte_array: base64ToIntArray(body.contract),
         public_key: hardcodedKey.public_key
-    })
-    console.log("response", contract_response);
-    checkError(ctx, contract_response);
-    ctx.body = contract_response.result;
-});
-
-router.post('/contract/:name/:methodName', async ctx => {
-    const body = ctx.request.body;
-    const sender = body.sender || hardcodedSender;
-    const nonce = body.nonce || await getNonce(ctx, sender);
-    const response = await submit_transaction_rpc(client, 'schedule_function_call', {
-        nonce: nonce,
-        originator_account_id: await hash(sender),
-        contract_account_id: await hash(ctx.params.name),
-        method_name: ctx.params.methodName,
-        args: [body.args]
-    });
+    }]);
     checkError(ctx, response);
     console.log("response", response);
     ctx.body = response.result;
 });
 
-router.post('/contract/view/:name/:methodName', async ctx => {
+// TODO: do not hardcode functionType
+router.post('/contract/:name/:functionType/:methodName', async ctx => {
+    console.log('JANE CALL 1');
     const body = ctx.request.body;
-    const response = await client.request('call_view_function', [{
+    // TODO: unhardcode
+    const a = {
+        "a": 1,
+        "b": 2
+    }
+
+    serializedArgs =  Array.from(BSON.serialize(a));
+
+
+    console.log('JANE CALL 2');
+
+    const params = [{
+        contract_account_id: await hash('magic'),
+        method_name: 'add',
+        args: serializedArgs
+    }];
+
+    const params2 = [{
+        contract_account_id: await hash('magic'),
+        method_name: 'sum_with_input',
+        args: [10, 0, 0, 0, 30, 0, 0, 0]
+    }];
+
+    console.log(params2);
+    console.log(params);
+
+
+    const response = await client.request('call_view_function', params);
+    console.log('JANE CALL 3');
+    console.log(response);
+    checkError(ctx, response);
+    ctx.body = response.result;
+});
+
+
+router.post('/contract/:name/:methodName', async ctx => {
+    const body = ctx.request.body;
+    const sender = body.sender || hardcodedSender;
+    const nonce = body.nonce || await getNonce(ctx, sender);
+    const serializedArgs =  Array.from(BSON.serialize(body.args));
+    const response = await client.request('schedule_function_call', [{
+        nonce: nonce,
+        originator_account_id: await hash(sender),
         contract_account_id: await hash(ctx.params.name),
         method_name: ctx.params.methodName,
-        args: body.args
+        args: serializedArgs
     }]);
     checkError(ctx, response);
     ctx.body = response.result;
