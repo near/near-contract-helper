e8a1263d3cc30a88a0d0bbba69a6167e3a34bbf9
Vladimir Grichina <vgrichina@gmail.com>
2018-12-16 22:19:35 -0800

    Sign transaction calling out to Rust process

--- app.js
+++ app.js
@@ -48,24 +48,30 @@ const hash = async str => {
     return bs58.encode(data);
 };
 
+const util = require('util');
+const execFile = util.promisify(require('child_process').execFile);
+const signTransaction = async (transaction) => {
+    const { stdout, stderr } = await execFile(
+        '../nearcore/target/debug/keystore',
+        ['sign_transaction', '--data', JSON.stringify(transaction)]);
+    return JSON.parse(stdout);
+};
+
 const submit_transaction_rpc = async (client, method, args) => {
     const response = await client.request(method, [args])
     if (response.error) {
         return response
     }
-    // TODO(#2): Sign transactions.
-    const res = await client.request('submit_transaction', [{
-        body: response.result.body,
-        sender_sig: [147, 237, 206, 27, 8, 112, 190, 228, 129, 23, 111, 153, 134, 68, 203, 152, 108, 105, 112, 238, 171, 200, 158, 83, 197, 20, 158, 214, 151, 190, 92, 135, 33, 158, 42, 159, 131, 80, 96, 201, 185, 254, 213, 46, 209, 11, 158, 2, 57, 161, 66, 222, 87, 192, 141, 53, 43, 198, 40, 107, 148, 218, 231, 7],
-    }])
-    return res
+
+    const transaction = response.result.body;
+    return await client.request('submit_transaction', [await signTransaction(transaction)])
 }
 
 router.post('/contract', async ctx => {
     const body = ctx.request.body;
     const sender = body.sender || hardcodedSender;
     const nonce = body.nonce || await getNonce(ctx, sender);
-    console.log(`Deploing ${body.receiver} contract`);
+    console.log(`Deploying ${body.receiver} contract`);
     const contract_response = await submit_transaction_rpc(client, 'deploy_contract', {
         nonce: nonce,
         sender_account_id: await hash(sender),
