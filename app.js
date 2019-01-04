9644bbb55e3f57f86490bede9c4590eb0a422b2d
Vladimir Grichina <vgrichina@gmail.com>
2019-01-04 14:29:40 -0800

    Configure eslint and autofix errors

--- app.js
+++ app.js
@@ -2,19 +2,19 @@
 const Koa = require('koa');
 const app = new Koa();
 
-const createError = require('http-errors')
-const body = require('koa-json-body')
+const createError = require('http-errors');
+const body = require('koa-json-body');
 const cors = require('@koa/cors');
 const BSON = require('bsonfy').BSON;
 const uuidV4 = require('uuid/v4');
 
 const hardcodedKey = {
-    public_key: "9AhWenZ3JddamBoyMqnTbp7yVbRuvqAv3zwfrWgfVRJE",
-    secret_key: "2hoLMP9X2Vsvib2t4F1fkZHpFd6fHLr5q7eqGroRoNqdBKcPja2jCrmxW9uGBLXdTnbtZYibWe4NoFtB4Bk7LWg6"
+    public_key: '9AhWenZ3JddamBoyMqnTbp7yVbRuvqAv3zwfrWgfVRJE',
+    secret_key: '2hoLMP9X2Vsvib2t4F1fkZHpFd6fHLr5q7eqGroRoNqdBKcPja2jCrmxW9uGBLXdTnbtZYibWe4NoFtB4Bk7LWg6'
 };
 
-const hardcodedSender = "bob";
-const defaultSender = "alice.near";
+const hardcodedSender = 'bob';
+const defaultSender = 'alice.near';
 const newAccountAmount = 5;
 
 const MAX_RETRIES = 3;
@@ -22,7 +22,7 @@ const POLL_TIME_MS = 500;
 
 app.use(require('koa-logger')());
 // TODO: Check what limit means and set appropriate limit
-app.use(body({ limit: '500kb', fallback: true }))
+app.use(body({ limit: '500kb', fallback: true }));
 // TODO: Don't use CORS in production on studio.nearprotocol.com
 app.use(cors({ credentials: true }));
 
@@ -36,25 +36,25 @@ const bs58 = require('bs58');
 const crypto = require('crypto');
 
 const InMemoryKeyStore = require('nearlib/test-tools/in_memory_key_store.js');
-const LocalNodeConnection = require('nearlib/local_node_connection')
+const LocalNodeConnection = require('nearlib/local_node_connection');
 const NearClient = require('nearlib/nearclient');
 const keyStore = new InMemoryKeyStore();
 keyStore.setKey(defaultSender, hardcodedKey);
-const localNodeConnection = new LocalNodeConnection("http://localhost:3030");
+const localNodeConnection = new LocalNodeConnection('http://localhost:3030');
 const nearClient = new NearClient(keyStore, localNodeConnection);
 const Account = require('nearlib/account');
 const account = new Account(nearClient);
 
 const base64ToIntArray = base64Str => {
     let data = Buffer.from(base64Str, 'base64');
-    return Array.prototype.slice.call(data, 0)
+    return Array.prototype.slice.call(data, 0);
 };
 
 const sha256 = data => {
     const hash = crypto.createHash('sha256');
     hash.update(data, 'utf8');
     return hash.digest();
-}
+};
 
 const request = async (methodName, params) => {
     try {
@@ -64,26 +64,26 @@ const request = async (methodName, params) => {
             .send(params);
         return JSON.parse(response.text);
     } catch(e) {
-        console.error("request error:", e.response.text);
+        console.error('request error:', e.response.text);
         throw e;
     }
-}
+};
 
 const viewAccount = async senderHash => {
     return await request('view_account', {
         account_id: senderHash,
     });
-}
+};
 
 const getNonce = async senderHash => {
     return (await viewAccount(senderHash)).nonce + 1;
-}
+};
 
 const sleep = timeMs => {
     return new Promise(function (resolve, reject) {
         setTimeout(resolve, timeMs);
     });
-}
+};
 
 const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
@@ -92,9 +92,9 @@ const signTransaction = async (transaction) => {
     const { stdout, stderr } = await execFile(
         '../nearcore/target/debug/keystore',
         ['sign_transaction',
-        '--data', stringifiedTxn,
-        '--keystore-path', '../nearcore/keystore/'
-    ]);
+            '--data', stringifiedTxn,
+            '--keystore-path', '../nearcore/keystore/'
+        ]);
     return JSON.parse(stdout);
 };
 
@@ -102,7 +102,7 @@ const submitTransaction = async (method, args) => {
     // TODO: Make sender param names consistent
     // TODO: https://github.com/nearprotocol/nearcore/issues/287
     const senderKeys = ['sender_account_id', 'originator_account_id', 'originator_id', 'sender'];
-    const sender = senderKeys.map(key => args[key]).find(it => !!it)
+    const sender = senderKeys.map(key => args[key]).find(it => !!it);
     const nonce = await getNonce(sender);
 
     for (let i = 0; i < MAX_RETRIES; i++) {
@@ -130,7 +130,7 @@ const submitTransaction = async (method, args) => {
         }
     }
     throw createError(504, `Transaction not accepted after ${MAX_RETRIES} retries`);
-}
+};
 
 router.post('/contract', async ctx => {
     const body = ctx.request.body;
@@ -140,7 +140,7 @@ router.post('/contract', async ctx => {
         contract_account_id: body.receiver,
         wasm_byte_array: base64ToIntArray(body.contract),
         public_key: hardcodedKey.public_key
-    })
+    });
 });
 
 router.post('/contract/:name/:methodName', async ctx => {
@@ -185,7 +185,7 @@ router.post('/account', async ctx => {
 
     const createAccountResponse =
         await account.createAccountWithRandomKey(newAccountName, 1, defaultSender);
-    createAccountResponse["accountName"] = newAccountName;
+    createAccountResponse['accountName'] = newAccountName;
     ctx.body = createAccountResponse;
 });
 
