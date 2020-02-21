2e8eab5964e46268e3f1ee24df7e81451af940a5
Vladimir Grichina <vgrichina@gmail.com>
2020-02-21 00:59:12 +0000

    Fix DB creation script for GitPod

--- scripts/create_dev_dbs.sh
+++ scripts/create_dev_dbs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-psql -c "create user helper with encrypted password 'helper';" -U postgres
-psql -c "create database accounts_test;" -U postgres
-psql -c "grant all privileges on database accounts_test to helper" -U postgres
-psql -c "create database accounts_development;" -U postgres
-psql -c "grant all privileges on database accounts_development to helper" -U postgres
+psql -c "create user helper with encrypted password 'helper';"
+psql -c "create database accounts_test;"
+psql -c "grant all privileges on database accounts_test to helper"
+psql -c "create database accounts_development;"
+psql -c "grant all privileges on database accounts_development to helper"
