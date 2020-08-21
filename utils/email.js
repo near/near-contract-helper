b5a56cf1c89eb5c739be8410bcccddaa0248ac1d
Matt Lockyer <mattdlockyer@gmail.com>
2020-08-20 17:32:00 -0700

    try background update

--- utils/email.js
+++ utils/email.js
@@ -258,12 +258,20 @@ const template = ({
       }
     </style>
   </head>
-  <body>
+  <body style="width: 100% !important;
+      height: 100% !important;
+      margin: 0;
+      padding: 0;
+      -webkit-font-smoothing: antialiased;
+      -webkit-text-size-adjust: none;
+      background: #25272A url('https://storage.googleapis.com/near-contract-helper/email-background.png') no-repeat;
+      background-size: contain;"
+  >
     <!-- The Email client preview part goes here -->
     <div style="display:none; font-size:1px; color:#333; line-height:1px; max-height:0; max-width:0; opacity:0; overflow:hidden;">
       ${ contentPreview}
     </div>
-    <table class="table-container" cellpadding="0" cellspacing="0" border="0" style="margin:0 auto;" background="https://storage.googleapis.com/near-contract-helper/email-background.png">
+    <table class="table-container" cellpadding="0" cellspacing="0" border="0" style="margin:0 auto;">
       <tr>
         <td height="40"></td>
       </tr>
