1c01cf63e2cdd3bcee6df844899325e383067dbd
Vladimir Grichina <vgrichina@gmail.com>
2020-12-19 15:22:32 -0800

    Add driver: 'memory'

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const NEW_ACCOUNT_AMOUNT = process.env.NEW_ACCOUNT_AMOUNT;
 
 const ratelimit = require('koa-ratelimit');
 
-router.post('/account', ratelimit({ duration: 15 * 60000, max: 10 }), async ctx => {
+router.post('/account', ratelimit({ driver: 'memory', duration: 15 * 60000, max: 10 }), async ctx => {
     if (!creatorKeyJson) {
         console.warn('ACCOUNT_CREATOR_KEY is not set up, cannot create accounts.');
         ctx.throw(404);
