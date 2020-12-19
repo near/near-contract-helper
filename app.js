edc762d42ee5f2795909124ea2af1ec1559b5ca9
Vladimir Grichina <vgrichina@gmail.com>
2020-12-19 15:09:59 -0800

    Add ratelimit of 10 creation requests per 15 minutes

--- app.js
+++ app.js
@@ -60,7 +60,9 @@ router.post('/2fa/verify', checkAccountOwnership, verifyCode);
 
 const NEW_ACCOUNT_AMOUNT = process.env.NEW_ACCOUNT_AMOUNT;
 
-router.post('/account', async ctx => {
+const ratelimit = require('koa-ratelimit');
+
+router.post(ratelimit({ duration: 15 * 60000, max: 10 }), '/account', async ctx => {
     if (!creatorKeyJson) {
         console.warn('ACCOUNT_CREATOR_KEY is not set up, cannot create accounts.');
         ctx.throw(404);
