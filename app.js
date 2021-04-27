72c12d05824e72342d7f9626c3204a11bf395578
Daryl Collins <daryl@darylcollins.com>
2021-04-27 02:00:01 -0700

    1659 - Move account creation route handlers into dedicated file in middleware directory

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const app = new Koa();
 
 const body = require('koa-json-body');
 const cors = require('@koa/cors');
-const nearAPI = require('near-api-js');
 
 const constants = require('./constants');
-const recaptchaValidator = require('./RecaptchaValidator');
 
 const { RECOVERY_METHOD_KINDS, SERVER_EVENTS } = constants;
 
@@ -56,8 +54,6 @@ const Router = require('koa-router');
 const router = new Router();
 
 const {
-    creatorKeyJson,
-    fundedCreatorKeyJson,
     withNear,
     checkAccountOwnership,
     checkAccountDoesNotExist,
@@ -79,87 +75,26 @@ router.post('/2fa/init', checkAccountOwnership, initCode);
 router.post('/2fa/send', checkAccountOwnership, sendNewCode);
 router.post('/2fa/verify', checkAccountOwnership, verifyCode);
 
-const NEW_ACCOUNT_AMOUNT = process.env.NEW_ACCOUNT_AMOUNT;
 
+const { createAccount, createFundedAccount } = require('./middleware/accountCreation');
 const ratelimit = require('koa-ratelimit');
 const ratelimitDb = new Map();
+
 router.post('/account', ratelimit({
     driver: 'memory',
     db: ratelimitDb,
     duration: 15 * 60000,
     max: 10,
     whitelist: () => process.env.NODE_ENV === 'test'
-}), async ctx => {
-    if (!creatorKeyJson) {
-        console.warn('ACCOUNT_CREATOR_KEY is not set up, cannot create accounts.');
-        ctx.throw(500, 'Service misconfigured; account creation is not available.');
-    }
+}), createAccount);
 
-    const { newAccountId, newAccountPublicKey } = ctx.request.body;
-
-    if (newAccountId.includes('dzarezenko')) {
-        ctx.throw(403);
-    }
-
-    const masterAccount = await ctx.near.account(creatorKeyJson.account_id);
-    ctx.body = await masterAccount.createAccount(newAccountId, newAccountPublicKey, NEW_ACCOUNT_AMOUNT);
-});
-
-// TODO: Adjust gas to correct amounts
-const MAX_GAS_FOR_ACCOUNT_CREATE = process.env.MAX_GAS_FOR_ACCOUNT_CREATE || '100000000000000';
-const FUNDED_ACCOUNT_BALANCE = process.env.FUNDED_ACCOUNT_BALANCE || nearAPI.utils.format.parseNearAmount('4');
-const FUNDED_NEW_ACCOUNT_CONTRACT_NAME = process.env.FUNDED_NEW_ACCOUNT_CONTRACT_NAME || 'near';
 router.post('/fundedAccount', ratelimit({
     driver: 'memory',
     db: ratelimitDb,
     duration: 15 * 60000,
     max: 10,
     whitelist: () => process.env.NODE_ENV === 'test'
-}), async ctx => {
-    if (!fundedCreatorKeyJson) {
-        console.warn('FUNDED_ACCOUNT_CREATOR_KEY is not set, cannot create funded accounts.');
-        ctx.throw(500, 'Funded account creation is not available.');
-    }
-
-    const {
-        newAccountId,
-        newAccountPublicKey,
-        recaptchaCode,
-    } = ctx.request.body;
-
-    if(!newAccountId) {
-        ctx.throw(400, 'newAccountId is required');
-    }
-
-    if(!newAccountPublicKey) {
-        ctx.throw(400, 'newAccountPublicKey is required');
-    }
-
-    if(!recaptchaCode) {
-        ctx.throw(400, 'recaptchaCode is required');
-    }
-
-    const { success, error } = await recaptchaValidator.validateRecaptchaCode(recaptchaCode, ctx.ip);
-
-    if (!success) {
-        const { statusCode, message } = error;
-        ctx.throw(statusCode, message);
-    }
-
-    const fundingAccount = await ctx.near.account(fundedCreatorKeyJson.account_id);
-
-    // TODO: Should the client get something different than the result of this call?
-    ctx.body = await fundingAccount.functionCall(
-        FUNDED_NEW_ACCOUNT_CONTRACT_NAME,
-        'create_account',
-        {
-            new_account_id: newAccountId,
-            new_public_key: newAccountPublicKey.replace(/^ed25519:/, '')
-        },
-        MAX_GAS_FOR_ACCOUNT_CREATE,
-        FUNDED_ACCOUNT_BALANCE
-    );
-});
+}), createFundedAccount);
 
 const { signURL } = require('./middleware/moonpay');
 router.get('/moonpay/signURL', signURL);
