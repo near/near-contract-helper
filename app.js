6947231b0e2d159ea621df9dd457e99be868cf85
Daryl Collins <daryl@darylcollins.com>
2021-04-26 22:20:59 -0700

    1659 - Initial cut of `/fundedAccount` route with recaptcha protection

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = new Koa();
 
 const body = require('koa-json-body');
 const cors = require('@koa/cors');
+const nearAPI = require('near-api-js');
 
 const constants = require('./constants');
-const { validateRecaptchaCode } = require('./recaptcha');
+const recaptchaValidator = require('./RecaptchaValidator');
 
 const { RECOVERY_METHOD_KINDS, SERVER_EVENTS } = constants;
 
@@ -56,6 +57,7 @@ const router = new Router();
 
 const {
     creatorKeyJson,
+    fundedCreatorKeyJson,
     withNear,
     checkAccountOwnership,
     checkAccountDoesNotExist,
@@ -101,6 +103,54 @@ router.post('/account', ratelimit({
 
     const masterAccount = await ctx.near.account(creatorKeyJson.account_id);
     ctx.body = await masterAccount.createAccount(newAccountId, newAccountPublicKey, NEW_ACCOUNT_AMOUNT);
+});
+
+// TODO: Adjust gas to correct amounts
+const MAX_GAS_FOR_ACCOUNT_CREATE = process.env.MAX_GAS_FOR_ACCOUNT_CREATE || '100000000000000';
+const FUNDED_ACCOUNT_BALANCE = process.env.FUNDED_ACCOUNT_BALANCE || nearAPI.utils.format.parseNearAmount('4');
+const FUNDED_NEW_ACCOUNT_CONTRACT_NAME = process.env.FUNDED_NEW_ACCOUNT_CONTRACT_NAME || 'near';
+router.post('/fundedAccount', ratelimit({
+    driver: 'memory',
+    db: ratelimitDb,
+    duration: 15 * 60000,
+    max: 10,
+    whitelist: () => process.env.NODE_ENV === 'test'
+}), async ctx => {
+    if (!fundedCreatorKeyJson) {
+        console.warn('FUNDED_ACCOUNT_CREATOR_KEY is not set, cannot create funded accounts.');
+        ctx.throw(500, 'Funded account creation is not available.');
+    }
+
+    const {
+        newAccountId,
+        newAccountPublicKey,
+        recaptchaCode,
+    } = ctx.request.body;
+
+    if (newAccountId.includes('dzarezenko')) {
+        ctx.throw(403);
+    }
+
+    const { success, error } = await recaptchaValidator.validateRecaptchaCode(recaptchaCode, ctx.ip);
+
+    if (!success) {
+        const { statusCode, message } = error;
+        ctx.throw(statusCode, message);
+    }
+
+    const fundingAccount = await ctx.near.account(fundedCreatorKeyJson.account_id);
+
+    // TODO: Should the client get something different than the result of this call?
+    ctx.body = await fundingAccount.functionCall(
+        FUNDED_NEW_ACCOUNT_CONTRACT_NAME,
+        'create_account',
+        {
+            new_account_id: newAccountId,
+            new_public_key: newAccountPublicKey.replace(/^ed25519:/, '')
+        },
+        MAX_GAS_FOR_ACCOUNT_CREATE,
+        FUNDED_ACCOUNT_BALANCE
+    );
 });
 
 const { signURL } = require('./middleware/moonpay');
