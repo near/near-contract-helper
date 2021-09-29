d7720537358547ddb6813edab6ff50de860fc889
Andy Haynes <andy@near.org>
2021-09-28 19:02:34 -0700

    Wrap middleware body in try/catch

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,38 +2,42 @@ const bunyan = require('bunyan');
 const SumoLogger = require('bunyan-sumologic');
 
 function logIdentityRequest(ctx, next) {
-    const {
-        body: {
-            kind,
-            identityKey,
-        },
-        ip,
-        method,
-        path,
-    } = ctx.request;
-
-    const logger = bunyan.createLogger({
-        name: 'near-contract-helper',
-        streams: [{
-            type: 'raw',
-            stream: new SumoLogger({
-                collector: process.env.SUMO_COLLECTOR_ID,
-                endpoint: process.env.SUMO_ENDPOINT,
-            }),
-        }],
-    });
-
-    logger.info({
-        identity: {
-            kind,
-            identityKey,
-        },
-        request : {
+    try {
+        const {
+            body: {
+                kind,
+                identityKey,
+            },
             ip,
             method,
             path,
-        },
-    });
+        } = ctx.request;
+
+        const logger = bunyan.createLogger({
+            name: 'near-contract-helper',
+            streams: [{
+                type: 'raw',
+                stream: new SumoLogger({
+                    collector: process.env.SUMO_COLLECTOR_ID,
+                    endpoint: process.env.SUMO_ENDPOINT,
+                }),
+            }],
+        });
+
+        logger.info({
+            identity: {
+                kind,
+                identityKey,
+            },
+            request : {
+                ip,
+                method,
+                path,
+            },
+        });
+    } catch (e) {
+        console.warn('Failed to log request', e);
+    }
 
     return next();
 }
