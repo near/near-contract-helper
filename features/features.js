36feb988894f7a1feef8f719616e0392025e84ed
Andy Haynes <andy@near.org>
2022-01-13 14:01:28 -0800

    feat: use initFeatureFlags method from feature flags package

--- features/features.js
+++ features/features.js
@@ -1,34 +1,7 @@
+const { initFeatureFlags } = require('@near-wallet/feature-flags');
+
 const Environments = require('./environments.json');
 const FeatureFlags = require('./flags.json');
-
-function initFeatureFlags({ flagState, currentEnvironment, environments }) {
-    if (typeof flagState !== 'object') {
-        throw Error('invalid flags');
-    }
-
-    if (!Object.values(environments).includes(currentEnvironment)) {
-        throw Error(`invalid environment: "${currentEnvironment}"`);
-    }
-
-    return new Proxy(flagState, {
-        get(flags, flag) {
-            if (flag === '__esModule') {
-                return { value: true };
-            }
-
-            const feature = flags[flag];
-            if (!feature) {
-                throw Error(`invalid feature: "${flag}"`);
-            }
-
-            if (!feature[currentEnvironment]) {
-                throw Error(`${flag} missing definition for environment: "${currentEnvironment}"`);
-            }
-
-            return feature[currentEnvironment].enabled;
-        },
-    });
-}
 
 const Features = initFeatureFlags({
     flagState: FeatureFlags,
