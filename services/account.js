4b088c3f1c8e912837029e285f91428ac298813d
Andy Haynes <andy@near.org>
2022-01-28 13:21:31 -0800

    feat: flagged method calls in AccountService

--- services/account.js
+++ services/account.js
@@ -1,20 +1,39 @@
+const {
+    createAccount,
+    deleteAccount,
+    getAccountById,
+    updateAccount,
+} = require('../db/methods/account');
+const { USE_DYNAMODB } = require('../features');
 const SequelizeAccounts = require('./sequelize/account');
 
 const AccountService = {
     createAccount(accountId, { fundedAccountNeedsDeposit } = {}) {
-        return SequelizeAccounts.createAccount(accountId, { fundedAccountNeedsDeposit });
+        if (!USE_DYNAMODB) {
+            return SequelizeAccounts.createAccount(accountId, { fundedAccountNeedsDeposit });
+        }
+        return createAccount({ accountId, fundedAccountNeedsDeposit });
     },
 
     async deleteAccount(accountId) {
-        return SequelizeAccounts.deleteAccount(accountId);
+        if (!USE_DYNAMODB) {
+            return SequelizeAccounts.deleteAccount(accountId);
+        }
+        return deleteAccount(accountId);
     },
 
     getAccount(accountId) {
-        return SequelizeAccounts.getAccount(accountId);
+        if (!USE_DYNAMODB) {
+            return SequelizeAccounts.getAccount(accountId);
+        }
+        return getAccountById(accountId);
     },
 
     async setAccountRequiresDeposit(accountId, requiresDeposit) {
-        return SequelizeAccounts.setAccountRequiresDeposit(accountId, requiresDeposit);
+        if (!USE_DYNAMODB) {
+            return SequelizeAccounts.setAccountRequiresDeposit(accountId, requiresDeposit);
+        }
+        return updateAccount(accountId, { fundedAccountNeedsDeposit: requiresDeposit });
     },
 };
 
