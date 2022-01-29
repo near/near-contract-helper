87dcaa117a367e86a0939ecd1d3e1832997c43e4
Andy Haynes <andy@near.org>
2022-01-28 20:50:40 -0800

    feat: DB methods for email domain blacklist

---
+++ db/methods/email_domain_blacklist/index.js
@@ -0,0 +1,7 @@
+const getEmailDomainBlacklistEntry = require('./get_by_domain_name');
+const updateEmailDomainBlacklistEntry = require('./update');
+
+module.exports = {
+    getEmailDomainBlacklistEntry,
+    updateEmailDomainBlacklistEntry,
+};
