2ea8362cca3a58c55a5bf7da2de6c8fc2b3aca5d
Andy Haynes <andy@near.org>
2022-01-28 20:51:02 -0800

    feat: flagged method calls in EmailDomainBlacklistService

--- services/email_domain_blacklist.js
+++ services/email_domain_blacklist.js
@@ -1,12 +1,25 @@
+const {
+    getEmailDomainBlacklistEntry,
+    updateEmailDomainBlacklistEntry,
+} = require('../db/methods/email_domain_blacklist');
+const { USE_DYNAMODB } = require('../features');
 const SequelizeEmailDomainBlacklists = require('./sequelize/email_domain_blacklist');
+
 
 const EmailDomainBlacklistService = {
     getDomainBlacklistEntry(domainName) {
-        return SequelizeEmailDomainBlacklists.getDomainBlacklistEntry(domainName);
+        if (!USE_DYNAMODB) {
+            return SequelizeEmailDomainBlacklists.getDomainBlacklistEntry(domainName);
+        }
+        return getEmailDomainBlacklistEntry(domainName);
     },
 
     updateDomainBlacklistEntry(blacklistEntry) {
-        return SequelizeEmailDomainBlacklists.updateDomainBlacklistEntry(blacklistEntry);
+        if (!USE_DYNAMODB) {
+            return SequelizeEmailDomainBlacklists.updateDomainBlacklistEntry(blacklistEntry);
+        }
+        const { domainName, ...entry } = blacklistEntry;
+        return updateEmailDomainBlacklistEntry(domainName, entry);
     },
 };
 
