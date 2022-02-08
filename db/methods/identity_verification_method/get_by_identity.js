bf78d7bf0f4ffd8941892ec8b2a825e73665b36c
Andy Haynes <andy@near.org>
2022-02-08 09:42:01 -0800

    fix: omit kind from identity lookup and align parameters

--- db/methods/identity_verification_method/get_by_identity.js
+++ db/methods/identity_verification_method/get_by_identity.js
@@ -1,10 +1,9 @@
 const { getDocument } = require('../../dynamo');
 const IdentityVerificationMethod = require('../../schemas/identity_verification_method');
 
-function getIdentityVerificationMethodByIdentity(identityKey, kind) {
+function getIdentityVerificationMethodByIdentity(identityKey) {
     return getDocument(IdentityVerificationMethod, {
         identityKey,
-        kind
     });
 }
 
