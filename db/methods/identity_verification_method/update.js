f26ab05a2399ed19882c2048832dc1ca15567669
Andy Haynes <andy@near.org>
2022-02-17 13:11:57 -0800

    refactor: kind is not a range key

--- db/methods/identity_verification_method/update.js
+++ db/methods/identity_verification_method/update.js
@@ -1,10 +1,9 @@
 const { updateDocument } = require('../../dynamo');
 const IdentityVerificationMethod = require('../../schemas/identity_verification_method');
 
-function updateIdentityVerificationMethod({ identityKey, kind }, attrs) {
+function updateIdentityVerificationMethod({ identityKey }, attrs) {
     return updateDocument(IdentityVerificationMethod, {
         identityKey,
-        kind,
         ...attrs,
     });
 }
