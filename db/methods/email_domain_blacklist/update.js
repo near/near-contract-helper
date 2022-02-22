13269df111205907061a3a7f0ba9ac2e4bc4635a
Andy Haynes <andy@near.org>
2022-02-22 09:57:29 -0800

    refactor: fix copy-pasta method name

--- db/methods/email_domain_blacklist/update.js
+++ db/methods/email_domain_blacklist/update.js
@@ -1,11 +1,11 @@
 const { updateDocument } = require('../../dynamo');
 const EmailDomainBlacklist = require('../../schemas/email_domain_blacklist');
 
-function updateAccount(domainName, blacklistEntry) {
+function updateEmailDomainBlacklist(domainName, blacklistEntry) {
     return updateDocument(EmailDomainBlacklist, {
         domainName,
         ...blacklistEntry,
     });
 }
 
-module.exports = updateAccount;
+module.exports = updateEmailDomainBlacklist;
