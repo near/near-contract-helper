976766100c2139b018d18123a885847e34a9b67b
Andy Haynes <andy@near.org>
2022-03-09 16:43:50 -0800

    chore(db): enumerate properties in update method

--- db/methods/identity_verification_method/update.js
+++ db/methods/identity_verification_method/update.js
@@ -1,10 +1,13 @@
 const { updateDocument } = require('../../dynamo');
 const IdentityVerificationMethod = require('../../schemas/identity_verification_method');
 
-function updateIdentityVerificationMethod({ uniqueIdentityKey }, attrs) {
+function updateIdentityVerificationMethod({ uniqueIdentityKey }, { claimed, identityKey, kind, securityCode }) {
     return updateDocument(IdentityVerificationMethod, {
         uniqueIdentityKey,
-        ...attrs,
+        claimed,
+        identityKey,
+        kind,
+        securityCode,
     });
 }
 
