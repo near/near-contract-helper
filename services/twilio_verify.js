970a774ea4f32c272454584fa4fe78773be17e81
Morgan McCauley <morgan@mccauley.co.nz>
2022-03-23 08:39:36 +1300

    refactor(2fa): Separate mock from `TwilioVerifyService` to its own class

--- services/twilio_verify.js
+++ services/twilio_verify.js
@@ -8,14 +8,23 @@ const USE_MOCK_TWILIO = process.env.USE_MOCK_TWILIO === 'true';
 
 const VERIFY_DELIVERY_CHANNEL = 'sms';
 
-module.exports = class TwilioVerifyService {
+class MockTwilioVerifyService {
     securityCodes = {};
 
-    constructor() {
-        if (USE_MOCK_TWILIO) {
-            return;
-        }
+    send({ to }, emitServerEvent) {
+        const securityCode = password.randomPassword({ length: 6, characters: password.digits });
 
+        this.securityCodes[to] = securityCode;
+        emitServerEvent(securityCode);
+    }
+
+    verify({ to, code }) {
+        return code === this.securityCodes[to];
+    }
+}
+
+class TwilioVerifyService {
+    constructor() {
         const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 
         this.verifyService = client
@@ -23,25 +32,14 @@ module.exports = class TwilioVerifyService {
             .services(VERIFY_SERVICE_SID);
     }
 
-    send({ to }, emitServerEvent) {
-        if (USE_MOCK_TWILIO) {
-            const securityCode = password.randomPassword({ length: 6, characters: password.digits });
-
-            this.securityCodes[to] = securityCode;
-            emitServerEvent(securityCode);
-
-            return;
-        }
-
+    send({ to }) {
         return this.verifyService.verifications.create({ to, channel: VERIFY_DELIVERY_CHANNEL });
     }
 
     async verify({ to, code }) {
-        if (USE_MOCK_TWILIO) {
-            return code === this.securityCodes[to];
-        }
-
         const { valid } = await this.verifyService.verificationChecks.create({ to, code });
         return valid;
     }
-};
+}
+
+module.exports = USE_MOCK_TWILIO ? MockTwilioVerifyService : TwilioVerifyService;
