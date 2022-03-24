a9c10ecaf5ec2dc7ee2edbdf8d84d3a34bc5623e
Morgan McCauley <morgan@mccauley.co.nz>
2022-03-25 08:39:00 +1300

    feat(2fa): Send user friendly messages for common twilio errors

--- services/twilio_verify.js
+++ services/twilio_verify.js
@@ -8,6 +8,51 @@ const USE_MOCK_TWILIO = process.env.USE_MOCK_TWILIO === 'true';
 
 const VERIFY_DELIVERY_CHANNEL = 'sms';
 
+// https://www.twilio.com/docs/api/errors/XXXXX
+const ERROR_RESPONSES_BY_CODE = {
+    20404: { status: 429, text: '2FA request expired, please try again' },
+    20429: { status: 429, text: '2FA rate limit reached, please try again later' },
+    60202: { status: 429, text: '2FA verification check limit reached, please try again later' },
+    60203: { status: 429, text: '2FA delivery limit reached, please try again later' },
+};
+
+const DEFAULT_ERROR_RESPONSE = {
+    status: 500,
+    text: 'Twilio Verify Error',
+};
+
+class TwilioVerifyService {
+    constructor() {
+        const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
+
+        this.verifyService = client
+            .verify
+            .services(VERIFY_SERVICE_SID);
+    }
+
+    async handleTwilioErrors(fn) {
+        try {
+            return await fn();
+        } catch (error) {
+            error.response = ERROR_RESPONSES_BY_CODE[error.code] || DEFAULT_ERROR_RESPONSE;
+            throw error;
+        }
+    }
+
+    send({ to }) {
+        return this.handleTwilioErrors(() => {
+            return this.verifyService.verifications.create({ to, channel: VERIFY_DELIVERY_CHANNEL });
+        });
+    }
+
+    verify({ to, code }) {
+        return this.handleTwilioErrors(async () => {
+            const { valid } = await this.verifyService.verificationChecks.create({ to, code });
+            return valid;
+        });
+    }
+}
+
 class MockTwilioVerifyService {
     securityCodes = {};
 
@@ -23,23 +68,5 @@ class MockTwilioVerifyService {
     }
 }
 
-class TwilioVerifyService {
-    constructor() {
-        const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
-
-        this.verifyService = client
-            .verify
-            .services(VERIFY_SERVICE_SID);
-    }
-
-    send({ to }) {
-        return this.verifyService.verifications.create({ to, channel: VERIFY_DELIVERY_CHANNEL });
-    }
-
-    async verify({ to, code }) {
-        const { valid } = await this.verifyService.verificationChecks.create({ to, code });
-        return valid;
-    }
-}
 
 module.exports = USE_MOCK_TWILIO ? MockTwilioVerifyService : TwilioVerifyService;
