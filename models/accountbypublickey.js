993b09d567a6f21939f75697671258240c82be2c
Andy Haynes <andy@near.org>
2022-03-28 16:26:14 -0700

    chore(db): remove sequelize modules

--- models/accountbypublickey.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-    const AccountByPublicKey = sequelize.define('AccountByPublicKey', {
-        accountId: DataTypes.STRING,
-        publicKey: DataTypes.STRING
-    }, {});
-    return AccountByPublicKey;
-};
