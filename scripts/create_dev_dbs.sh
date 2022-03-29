a16cab95b6d3417588ef73c01aa03c592db58b35
Andy Haynes <andy@near.org>
2022-03-28 17:24:20 -0700

    chore(db): remove misc postgres references

--- scripts/create_dev_dbs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-psql -c "create user helper with encrypted password 'helper';"
-psql -c "create user indexer with encrypted password 'indexer';"
-psql -c "create database accounts_test;"
-psql -c "grant all privileges on database accounts_test to helper"
-psql -c "create database accounts_development;"
-psql -c "grant all privileges on database accounts_development to helper"
-psql -c "create database indexer;"
-psql -c "grant all privileges on database indexer to indexer"
